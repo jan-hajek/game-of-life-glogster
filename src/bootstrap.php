57fd9cfc5d0382e0fd255cda02d29dc2bac0b773
Jan Hájek <hajek.mail@gmail.com>
2015-12-09 18:34:08 +0100

    autoload via composer

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,3 @@ define('SRC_DIR', __DIR__);
 define('PROJECT_DIR', dirname(__DIR__));
 
 require_once PROJECT_DIR . '/vendor/autoload.php';
-
-spl_autoload_register(function ($class) {
-	require_once SRC_DIR . '/' . str_replace('\\', '/', $class) . '.php';
-});
